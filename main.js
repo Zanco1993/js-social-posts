caae062cfe3d35239aef3360cbfade9bd74682e6
Zanco1993 <zanconatofabio@gmail.com>
2022-01-11 14:04:29 +0100

    milestone 1 completato

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /*
 Descrizione
-Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+Ricreiamo un feed social aggiungendo al layout di base fornito,
+ il nostro script JS in cui:
 
 Milestone 1
 Creiamo il nostro array di oggetti che rappresentano ciascun post.
@@ -12,13 +13,16 @@ data in formato americano (mm-gg-yyyy),
 testo del post,
 immagine (non tutti i post devono avere una immagine),
 numero di likes.
-Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>
+Per le immagini va bene utilizzare qualsiasi servizio di placeholder
+ad es. Unsplash (https://unsplash.it/300/300?image=<id>
 
 Milestone 2
-Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
+Prendendo come riferimento il layout di esempio presente nell’html, 
+stampiamo i post del nostro feed.
 
 Milestone 3
-Se clicchiamo sul tasto “Mi Piace” cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+Se clicchiamo sul tasto “Mi Piace” cambiamo il colore al testo del bottone
+e incrementiamo il counter dei likes relativo.
 Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
 
 BONUS
@@ -31,3 +35,37 @@ Ragioniamo come sempre a step.
 Prima scriviamo nei commenti la logica in italiano e poi traduciamo in codice. console.log() è nostro amico.
 Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzioni più piccole.
 */
+
+//--------------------------------------------------------------------------------
+
+// creo l'array di oggetti
+
+const social = [
+    {
+        id: 1,
+        name: "Andrea",
+        profileImage: "https://unsplash.it/300/300?image=15",
+        dataAmerican: 05-01-2021,
+        text: "Ciao sono Andrea",
+        postImage: "https://unsplash.it/600/300?image=171",
+        likes: 20
+    },
+    {
+        id: 2,
+        name: "Marco",
+        profileImage: "https://unsplash.it/300/300?image=15",
+        dataAmerican: 03-22-2021,
+        text: "Ciao mi chiamo Marco",
+        postImage: "https://unsplash.it/600/300?image=171",
+        likes: 30
+    },
+    {
+        id: 3,
+        name: "Fabio",
+        profileImage: "https://unsplash.it/300/300?image=15",
+        dataAmerican: 02-03-2021,
+        text: "Ciao mi chiamo Fabio",
+        postImage: "https://unsplash.it/600/300?image=171",
+        likes: 40
+    }
+];
