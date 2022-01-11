6b8c77071291e2d0a31d9b304e670d89f794106c
Zanco1993 <zanconatofabio@gmail.com>
2022-01-11 14:31:55 +0100

    milestone 2 completa

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ const social = [
         id: 1,
         name: "Andrea",
         profileImage: "https://unsplash.it/300/300?image=15",
-        dataAmerican: 05-01-2021,
+        dataAmerican: "05-01-2021",
         text: "Ciao sono Andrea",
         postImage: "https://unsplash.it/600/300?image=171",
         likes: 20
@@ -53,19 +53,61 @@ const social = [
     {
         id: 2,
         name: "Marco",
-        profileImage: "https://unsplash.it/300/300?image=15",
-        dataAmerican: 03-22-2021,
+        profileImage: "https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=",
+        dataAmerican: "03-22-2021",
         text: "Ciao mi chiamo Marco",
-        postImage: "https://unsplash.it/600/300?image=171",
+        postImage: "https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=",
         likes: 30
     },
     {
         id: 3,
         name: "Fabio",
-        profileImage: "https://unsplash.it/300/300?image=15",
-        dataAmerican: 02-03-2021,
+        profileImage: "https://i.picsum.photos/id/928/300/300.jpg?hmac=QWFJOwXiovf5nsBF_GT3zPKfE-W2vrteEo3m0NcfYCM",
+        dataAmerican: "02/03/2021",
         text: "Ciao mi chiamo Fabio",
-        postImage: "https://unsplash.it/600/300?image=171",
+        postImage: "https://media.istockphoto.com/photos/rocky-mountains-and-autumnal-forest-with-colorful-trees-high-mountain-picture-id1282522992?b=1&k=20&m=1282522992&s=170667a&w=0&h=W_7cRAwDTWOqqXF3g9zwOaCMgol1-Vb4rYtlqBbJMYE=",
         likes: 40
     }
 ];
+
+// mi posiziono in container e cammino nell'array di oggetti
+// inserisco dove mi serve, le varie informazioni dell'utente
+
+const containerHtml = document.getElementById("container");
+
+for(let i=0; i < social.length; i++) {
+    containerHtml.innerHTML += 
+   `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${social[i].profileImage}" alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${social[i].name}</div>
+                    <div class="post-meta__time">Pubblicato il ${social[i].dataAmerican}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${social[i].text}</div>
+        <div class="post__image">
+            <img src="${social[i].postImage}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${social[i].likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+    `
+
+}
